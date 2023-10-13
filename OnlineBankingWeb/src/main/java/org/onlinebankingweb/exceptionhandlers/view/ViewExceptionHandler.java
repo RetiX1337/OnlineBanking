18aa55393b589b17bcb41e8a03f3add62e8fbfca
Matvii Butenov <matvej1888@gmail.com>
2023-10-13 12:47:39 +0200

    added error handling for view

---
+++ OnlineBankingWeb/src/main/java/org/onlinebankingweb/exceptionhandlers/view/ViewExceptionHandler.java
@@ -0,0 +1,14 @@
+package org.onlinebankingweb.exceptionhandlers.view;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice(basePackages = "org.onlinebankingweb.controllers.view")
+public class ViewExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public String handleAnyException(Model model) {
+        model.addAttribute("exceptionMessage", "Some unexpected error has occurred...");
+        return "error-page";
+    }
+}
